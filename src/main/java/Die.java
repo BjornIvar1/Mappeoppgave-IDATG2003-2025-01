29e7f66a3fc1abc8956404b362c15da14299b7a8
bjornivar <bihoie@stud.ntnu.no>
2025-02-11 21:30:18 +0100

    Updated Die: - Created JavaDoc - Fixed CheckStyle and SonarQube violations - Refactored Random to an Object

--- src/main/java/Die.java
+++ src/main/java/Die.java
@@ -1,13 +1,33 @@
 import java.util.Random;
 
+/**
+ * The class Die represents a six sided die,
+ * that can generate a number between 1-6.
+ *
+ * @since 0.0.1
+ * @author Bjørn
+ * @version 0.0.1
+ */
 public class Die {
   private int lastRolledValue;
+    private final Random random = new Random();
 
+  /**
+   * Rolls a die generating a random number between 1-6,
+   * using {@link Random}.
+   *
+   * @return the value of the last rolled
+   */
   public int roll() {
-    lastRolledValue = new Random().nextInt(6)+1;
+    lastRolledValue = random.nextInt(1, 7);
     return lastRolledValue;
   }
 
+  /**
+   * Returns the last rolled value.
+   *
+   * @return the value of the last rolled
+   */
   public int getValue() {
     return lastRolledValue;
   }
