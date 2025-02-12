38e40558786abaa0c369ee677e7b3a62ce4cafdb
bjornivar <bihoie@stud.ntnu.no>
2025-02-12 12:19:55 +0100

    Created methods for Tile  - Tile(): Made a constructor for tile  - landPlayer(): Made a method to perform and action on a player  - leavePlayer(): Not finished yet  - setNextTile(): Sets the next tile  - getNextTile(): Gets the next tile

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -1,5 +1,63 @@
+/**
+ * Class Tile represents a tile on a game board.
+ * Each tile has its own ID, and a tile can perform an action on a player.
+ *
+ * @since 0.0.1
+ * @author Bjørn
+ * @version 0.0.1
+ */
 public class Tile {
   private Tile nextTile;
-  private int TileId;
+  private int tileId;
   private TileAction landAction;
+
+  /**
+   * Constructor for creating Tile with an unique ID.
+   *
+   * @param tileId the id for the tile
+   */
+  public Tile(int tileId) {
+    this.tileId = tileId;
+  }
+
+  /**
+   * Performs an action on a player when a player lands on the tile.
+   * The action is performed by the landAction.
+   *
+   * @param player the player to perform an action on
+   */
+  public void landPlayer(Player player) {
+    if (player != null) {
+      landAction.perform(player);
+    }
+  }
+
+  /**
+   * Leaveplayer...
+   * method for a player leaving the game?
+   * method for player when no more tiles = winning the game?
+   *
+   * @param player the player to leave...
+   */
+  public void leavePlayer(Player player) {
+
+  }
+
+  /**
+   * Sets the next tile on the game board.
+   *
+   * @param nextTile the next tile on the board
+   */
+  public void setNextTile(Tile nextTile) {
+    this.nextTile = nextTile;
+  }
+
+  /**
+   * Returns the next tile.
+   *
+   * @return the next tile on the board
+   */
+  public Tile getNextTile() {
+    return nextTile;
+  }
 }
