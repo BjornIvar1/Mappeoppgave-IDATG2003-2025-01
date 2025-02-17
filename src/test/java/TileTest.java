728cd2b7473e052cd7f505af1793ff5fedec1a5c
bjornivar <bihoie@stud.ntnu.no>
2025-02-17 14:40:40 +0100

    Created test classes for the accessor methods for tileId

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -10,9 +10,9 @@ class TileTest {
 
   @BeforeEach
   void setUp() {
-    tile1 = new Tile(1);
+    tile1 = new Tile(0);
     tile2 = new Tile(1);
-    tile3 = new Tile(1);
+    tile3 = new Tile(2);
   }
 
   @Test
@@ -23,5 +23,20 @@ class TileTest {
     assertEquals(tile2, tile1.getNextTile());
     assertEquals(tile2, tile1.getNextTile());
     assertNull(tile3.getNextTile());
+  }
+
+  @Test
+  void getCorrectTileId() {
+    assertEquals(0, tile1.getTileId());
+    assertEquals(1, tile2.getTileId());
+    assertEquals(2, tile3.getTileId());
+  }
+
+  @Test
+  void setIncorrectTileId() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      tile1.setTileId(-1);
+    });
+    assertEquals("Tile ID must be a positive integer.", exception.getMessage());
   }
 }
