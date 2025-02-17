cfcefb43ea8f27a97bcf797379169d3f73f33ac0
bjornivar <bihoie@stud.ntnu.no>
2025-02-17 15:15:38 +0100

    Fixed the test class BoardTest.java  - Test the methods from the Board class

--- src/test/java/BoardTest.java
+++ src/test/java/BoardTest.java
@@ -6,31 +6,36 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-  private Map<Integer, Tile> tiles;
+  Board board;
+  Map<Integer, Tile> tiles;
+
   Tile tile1;
   Tile tile2;
   Tile tile3;
 
   @BeforeEach
   void setUp() {
-    tiles = new HashMap<Integer, Tile>();
+    board = new Board();
+    tiles = new HashMap<>();
 
     tile1 = new Tile(1);
     tile2 = new Tile(2);
     tile3 = new Tile(3);
-
-    tile1.setNextTile(tile2);
-    tile2.setNextTile(tile3);
   }
 
   @Test
   void positiveTestToAddTile() {
-    tiles.put(1, tile1); //ACT
-    tiles.put(2, tile2);
-    tiles.put(3, tile3);
-    assertEquals(tile1, tiles.get(1), "Was successfully"); //ASSERT
-    assertEquals(tile2, tiles.get(2), "Was successfully");
-    assertEquals(tile3, tiles.get(3), "Was successfully");
+    board.addTile(tile1);
+    board.addTile(tile2);
+    board.addTile(tile3);
+
+    assertEquals(tile1, board.getTile(1));
+    assertEquals(tile2, board.getTile(2));
+    assertEquals(tile3, board.getTile(3));
   }
 
+  @Test
+  void negativeTestGetTile() {
+    assertNull(board.getTile(4));
+  }
 }
