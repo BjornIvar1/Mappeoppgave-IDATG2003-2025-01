d99c20345991bc413f55bbe4c99919c482964e6e
bjornivar <bihoie@stud.ntnu.no>
2025-02-19 10:38:04 +0100

    Initializing a test for a board game.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    BoardGame boardGame = new BoardGame();
+    Player player1 = new Player("Jon");
+    Player player2 = new Player("Mona");
+    Player player3 = new Player("Arpit");
+    Player player4 = new Player("Bjørn");
+    boardGame.addPlayer(player1);
+    boardGame.addPlayer(player2);
+    boardGame.addPlayer(player3);
+    boardGame.addPlayer(player4);
+    boardGame.createBoard(101);
+
+    boardGame.play();
   }
 }
