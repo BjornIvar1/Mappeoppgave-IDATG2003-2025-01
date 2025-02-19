ba9913fd85e03f4c0d63948425b52ea36c3d485e
bjornivar <bihoie@stud.ntnu.no>
2025-02-19 10:48:35 +0100

    Updated BoardGame.java  - Added JavaDoc  - Fixed CheckStyle violations  - Added createDice() method for creating a dice  - Added createBoard() method for creating the board  - Added play() method to run the game and determine a winner

--- src/main/java/BoardGame.java
+++ src/main/java/BoardGame.java
@@ -2,47 +2,102 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Class that represents the whole game.
+ * The class {@code BoardGame} represents the whole game.
+ * It includes methods for: adding a player, creating dice, creating a board
+ * and playing the game.
  *
- * @version 0.0.2
+ * @since 0.0.1
+ * @author Bjørn
+ * @version 0.1.0
  */
 public class BoardGame {
+  private List<Player>players;
   private Board board;
   private Player currentPlayer;
-  private List<Player>players;
+  private Dice dice;
 
   /**
-   * A constructor the class BoardGame.
+   * A constructor the {@code BoardGame} class.
    */
   public BoardGame() {
-    board = new Board();
     players = new ArrayList<>();
   }
 
   /**
-   * A methods that allows the user to add a new player.
+   * A method for adding a player to the board game.
+   * The player can not already exist or be {@code null}
    *
-   * @param player a new player.
+   * @param player the player to add
    */
-  private void addPlayer(Player player) {
-    for (Player p : players) {
-      if (!p.equals(player)) {
-        players.add(player);
-      }
+  public void addPlayer(Player player) {
+    if (!players.contains(player) && player != null) {
+      players.add(player);
     }
   }
 
-  /*
-  private void play() {
-
+  /**
+   * Creates a new set of dice with a specific amount,
+   * using the constructor from the class Dice.
+   *
+   * @param numberOfDice the number of dice to create
+   */
+  public void createDice(int numberOfDice) {
+    dice = new Dice(numberOfDice);
   }
 
+  /**
+   * Creates the board with a specified amount of tiles.
+   * Each tile is linked together forming the board game,
+   * using the {@link Tile#setNextTile(Tile)}.
+   *
+   * @param numberOfTiles the number of tiles to create
    */
+  public void createBoard(int numberOfTiles) {
+    board = new Board();
 
-  /*
-  private void getWinner(Player player) {
-    currentPlayer = player;
+    Tile previousTile = null;
+    for (int i = 0; i < numberOfTiles; i++) {
+      Tile tile = new Tile(i);
+      board.addTile(tile);
+
+      if (previousTile != null) {
+        previousTile.setNextTile(tile);
+      }
+      previousTile = tile;
+    }
   }
 
+  /**
+   * Simple board game for testing...
    */
+  public void play() {
+    boolean winner = false;
+    int round = 0;
+    createDice(8);
+    System.out.println("The following players are playing the game:");
+    for (Player player : players) {
+      System.out.println(" -" + player.getName());
+      player.placeOnTile(board.getTile(0));
+    }
+
+    while (!winner) {
+      round++;
+      System.out.println();
+      System.out.println("Runde: " + round);
+      for (Player player : players) {
+        int steps = dice.roll();
+        player.move(steps);
+
+        Tile currentTile = player.getCurrentTile();
+
+        if (currentTile.getTileId() == board.getTiles().size()-1) {
+          System.out.println("And the winner is... " + player.getName());
+          winner = true;
+          break;
+        }
+
+        System.out.println(player.getName() + " on tile " + player.getCurrentTile().getTileId());
+      }
+    }
+  }
 }
