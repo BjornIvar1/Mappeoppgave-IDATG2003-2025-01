2ef48eb19a3207292fac037914ec9c32ad2158f6
arpit <arpitsa@stud.ntnu.no>
2025-02-24 15:24:23 +0100

    PlayerTest
    Made 2 new test methods in PlayerTest, placeOnTilePositiveTest and movePositiveTest

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -3,9 +3,16 @@ import org.junit.jupiter.api.*;
 
 class PlayerTest {
   Player player1;
+  Tile tile1;
+  Tile tile2;
+
   @BeforeEach
   void setUp() {
     player1 = new Player("John");
+    tile1 = new Tile(2);
+    tile2 = new Tile(3);
+
+    tile1.setNextTile(tile2);
   }
 
   @Test
@@ -17,6 +24,19 @@ class PlayerTest {
   void setNameNegativeTest(){
     assertThrows(IllegalArgumentException.class, () -> player1.setName(" "));
     assertThrows(IllegalArgumentException.class, () -> player1.setName(null));
+  }
+
+  @Test
+  void placeOnTilePositiveTest () {
+    player1.placeOnTile(tile1);
+    assertEquals(2, player1.getCurrentTile().getTileId());
+  }
+
+  @Test
+  void movePositiveTest () {
+    player1.placeOnTile(tile1);
+    player1.move(1);
+    assertEquals(3, player1.getCurrentTile().getTileId());
   }
 
 }
