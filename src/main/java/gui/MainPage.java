d57e0f2124496fc95b63d01083a9e7ea844516b8
arpit <arpitsa@stud.ntnu.no>
2025-03-12 20:48:48 +0100

    Created the Main page with a set of methods.
    It possible to see the logo, menubar and a button.

---
+++ src/main/java/gui/MainPage.java
@@ -0,0 +1,115 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/**
+ * The {@code MainPage} class creates a graphical user interface for interacting that
+ * allows the user to enter and leave the game.
+ *
+ * @since 0.0.1
+ * @author Arpit & Bjørn
+ * @version 0.0.1
+ */
+public class MainPage extends Application {
+
+  /**
+   * Starts the JavaFX application and initializes the user interface.
+   *
+   * @param primaryStage the primary stage for this application
+   * @throws Exception if an error occurs during initialization
+   */
+  @Override
+  public void start(Stage primaryStage) {
+    BorderPane root = new BorderPane();
+    root.setCenter(buttonAndImage());
+    root.setTop(createmenuBar());
+
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("3Spill");
+    primaryStage.show();
+  }
+
+  /**
+   * Creates a menubar for the user to easily exit the game.
+   * @return
+   */
+  private MenuBar createmenuBar() {
+    MenuBar menuBar = new MenuBar();
+    Menu fileitem = new Menu("file");
+
+    MenuItem closeItem = new MenuItem("Exit");
+    closeItem.setOnAction(e -> System.exit(0));
+
+    fileitem.getItems().add(closeItem);
+    menuBar.getMenus().addAll(fileitem);
+    return menuBar;
+  }
+
+  /**
+   * Aligns the button and Logo under each other.
+   *
+   * @return buttonAndImage.
+   */
+  private Pane buttonAndImage() {
+    VBox buttonAndImage = new VBox();
+    buttonAndImage.setAlignment(Pos.CENTER);
+    buttonAndImage.setSpacing(10);
+    buttonAndImage.setPadding(new Insets(10, 10, 10, 10));
+    buttonAndImage.getChildren().addAll(imagePaneCreate(), startButtonPaneCreate());
+    return buttonAndImage;
+  }
+
+  /**
+   * Fetches the logo and adds it to the interface, so the
+   * user can see what application they are on.
+   *
+   * @return imagePane.
+   */
+  private FlowPane imagePaneCreate() {
+    FlowPane imagePane = new FlowPane();
+    ImageView image = new ImageView("3spill.png");
+    imagePane.getChildren().add(image);
+    imagePane.setAlignment(Pos.CENTER);
+    return imagePane;
+  }
+
+  /**
+   * Adds a button, so the player can go to next page
+   * and begin playing the game.
+   *
+   * @return buttonPane.
+   */
+  private FlowPane startButtonPaneCreate() {
+    FlowPane buttonPane = new FlowPane();
+    Button startButton = new Button("Start");
+    startButton.prefHeight(30);
+    startButton.prefWidth(300);
+    buttonPane.getChildren().addAll(startButton);
+    buttonPane.setAlignment(Pos.CENTER);
+    return buttonPane;
+  }
+
+  /**
+   * The main entry point of the application.
+   *
+   * @param args command-line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+
+
+}
+
