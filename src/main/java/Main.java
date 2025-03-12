f0a07966a64a4855238403d779eb6472a330661c
arpit <arpitsa@stud.ntnu.no>
2025-03-12 20:48:37 +0100

    Created the Main page with a set of methods.
    It possible to see the logo, menubar and a button.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 import engine.BoardGame;
+import gui.MainPage;
 import model.Player;
 
 public class Main {
   public static void main(String[] args) {
     BoardGame boardGame = new BoardGame();
-
+    MainPage.main(args);
     boardGame.createBoard(90); // Creates a standard board of 90 linked tiles
     boardGame.createDice(2); // Creates 2 Dice
     boardGame.addPlayer(new Player("Arne", boardGame));
