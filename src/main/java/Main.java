ddc13c3550862cb07eee3254daac5f6fa321c90a
bjornivar <bihoie@stud.ntnu.no>
2025-03-25 13:15:17 +0100

    Edited the Main.java class for testing BoardFileReaderGson

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,26 @@
-import engine.SnakeAndLaddersGame;
-import gui.MainPage;
+import engine.BoardGame;
+import filehandler.BoardFileReaderGson;
+import filehandler.BoardFileeReader;
+import model.Board;
 import model.Player;
+
+import java.io.IOException;
+import java.nio.file.Path;
 
 public class Main {
   public static void main(String[] args) {
-    SnakeAndLaddersGame game = new SnakeAndLaddersGame();
-    MainPage.main(args);
-    game.createDice(2);
-    game.addPlayer(new Player("Arne", game));
-    game.addPlayer(new Player("Ivar", game));
-    game.addPlayer(new Player("Majid", game));
-    game.addPlayer(new Player("Atle", game));
-    game.play();
+    BoardFileeReader reader = new BoardFileReaderGson();
+    try {
+      Board board = reader.readBoard(Path.of("board.json"));
+      BoardGame boardGame = new BoardGame(board);
+      boardGame.createDice(1);
+      boardGame.addPlayer(new Player("Arne", boardGame));
+      boardGame.addPlayer(new Player("Ivar", boardGame));
+      boardGame.addPlayer(new Player("Majid", boardGame));
+      boardGame.addPlayer(new Player("Atle", boardGame));
+      boardGame.play();
+    } catch (IOException e) {
+      System.out.println("Could not read board from file: " + e.getMessage());
+    }
   }
 }
