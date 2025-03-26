259a8623bcea22343218c7d5bcb6fca18e57cd60
bjornivar <bihoie@stud.ntnu.no>
2025-03-26 12:47:45 +0100

    Enhance writeBoard method to include game name and description, and refactor tile writing logic

--- src/main/java/filehandler/BoardFileWriterGson.java
+++ src/main/java/filehandler/BoardFileWriterGson.java
@@ -1,7 +1,5 @@
 package filehandler;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import model.Board;
 import model.Tile;
 
@@ -16,40 +14,50 @@ public class BoardFileWriterGson implements BoardFileWriter{
   public void writeBoard(Board board, Path filePath) throws IOException {
     try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()))) {
       writer.write("{\n");
-      //TODO Name and description
-      writer.write("\"rows\": " + board.getRows() + ",\n");
-      writer.write("\"columns\": " + board.getColumns() + ",\n");
-      writer.write("\"tiles\": \n");
-      writer.write("[\n");
-      Iterator<Tile> iterator = board.getTiles().values().iterator();
-      while (iterator.hasNext()) {
-        Tile tile = iterator.next();
-        writer.write("    {");
-        writer.write("\"id\": " + tile.getTileId() + ", ");
-        writer.write("\"x\": " + tile.getXcoordinate() + ", ");
-        writer.write("\"y\": " + tile.getYcoordinate());
+      writer.write("  \"name\": \"" + "Snakes and Ladders" + "\",\n");
+      writer.write("  \"description\": \"" + "A simple snakes and ladders board game" + "\",\n");
+      writer.write("  \"rows\": " + board.getRows() + ",\n");
+      writer.write("  \"columns\": " + board.getColumns() + ",\n");
+      writer.write("  \"tiles\": [\n");
 
-        if (tile.getNextTile() != null) {
-          writer.write(", " + "\"nextTile\": " + tile.getNextTile().getTileId());
-        }
+      writeTiles(board, writer);
 
-        if (tile.getLandAction() != null) {
-          writer.write(", " + "\"action\": { ");
-          writer.write("\"type\": \"" + "LadderAction" + "\"");
-          writer.write(", " + "\"description\": \"" + tile.getLandAction().getDescription() + "\"");
-          writer.write(", " + "\"destinationTileId\": " + tile.getLandAction().getDestinationTile());
-          writer.write("}");
-        }
-        writer.write("}");
-
-        if (iterator.hasNext()) {
-          writer.write(",\n");
-        } else {
-          writer.write("\n");
-        }
-      }
       writer.write("  ]\n");
       writer.write("}");
     }
+  }
+
+  private static void writeTiles(Board board, BufferedWriter writer) throws IOException {
+    Iterator<Tile> iterator = board.getTiles().values().iterator();
+    while (iterator.hasNext()) {
+      Tile tile = iterator.next();
+      writer.write("    {");
+      writer.write("\"id\": " + tile.getTileId() + ", ");
+      writer.write("\"x\": " + tile.getXcoordinate() + ", ");
+      writer.write("\"y\": " + tile.getYcoordinate());
+
+      if (tile.getNextTile() != null) {
+        writer.write(", " + "\"nextTile\": " + tile.getNextTile().getTileId());
+      }
+
+      if (tile.getLandAction() != null) {
+        writeLandAction(writer, tile);
+      }
+      writer.write("}");
+
+      if (iterator.hasNext()) {
+        writer.write(",\n");
+      } else {
+        writer.write("\n");
+      }
+    }
+  }
+
+  private static void writeLandAction(BufferedWriter writer, Tile tile) throws IOException {
+    writer.write(", " + "\"action\": { ");
+    writer.write("\"type\": \"" + "LadderAction" + "\"");
+    writer.write(", " + "\"description\": \"" + tile.getLandAction().getDescription() + "\"");
+    writer.write(", " + "\"destinationTileId\": " + tile.getLandAction().getDestinationTile());
+    writer.write("}");
   }
 }
