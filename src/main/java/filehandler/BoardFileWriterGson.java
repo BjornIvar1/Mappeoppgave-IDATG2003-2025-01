c6425afe1149f576fe23f3c28630055f399e477c
bjornivar <bihoie@stud.ntnu.no>
2025-03-26 12:33:17 +0100

    Created the first draft for writeBoard method in BoardFileWriterGson

---
+++ src/main/java/filehandler/BoardFileWriterGson.java
@@ -0,0 +1,48 @@
+package filehandler;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import model.Board;
+import model.Tile;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class BoardFileWriterGson implements BoardFileWriter{
+  @Override
+  public void writeBoard(Board board, Path filePath) throws IOException {
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath.toFile()))) {
+      writer.write("{\n");
+      //TODO Name and description
+      writer.write("\"rows\": " + board.getRows() + ",\n");
+      writer.write("\"columns\": " + board.getColumns() + ",\n");
+      writer.write("\"tiles\": \n");
+      writer.write("[\n");
+
+      for (Tile tile : board.getTiles().values()) {
+        writer.write("    {");
+        writer.write("\"id\": " + tile.getTileId() + ", ");
+        writer.write("\"x\": " + tile.getXcoordinate() + ", ");
+        writer.write("\"y\": " + tile.getYcoordinate());
+        if (tile.getNextTile() == null) {
+          writer.write("} \n");
+        } else {
+          writer.write(", " + "\"nextTile\": " + tile.getNextTile().getTileId());
+        }
+        if (tile.getLandAction() != null) {
+          writer.write(", " + "\"action\": { ");
+          writer.write("\"type\": \"" + "LadderAction" + "\"");
+          writer.write(", " + "\"description\": \"" + tile.getLandAction().getDescription() + "\"");
+          writer.write(", " + "\"destinationTileId\": " + tile.getLandAction().getDestinationTile());
+          writer.write("}}," + "\n");
+        } else if (tile.getNextTile() != null) {
+          writer.write("},\n");
+        }
+      }
+      writer.write("  ]\n");
+      writer.write("}");
+    }
+  }
+}
