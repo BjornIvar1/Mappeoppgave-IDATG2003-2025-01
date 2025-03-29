500dd77c03988e7b8d06fddc4ef7afad4276b257
arpit <arpitsa@stud.ntnu.no>
2025-03-29 16:21:57 +0100

    Added a csvReader method in main class for testing purposes.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 import engine.BoardGame;
 import engine.SnakeAndLaddersGame;
+import filehandler.PlayerFileReader;
 import gui.MainPage;
 import model.Player;
 
 public class Main {
+
   public static void main(String[] args) {
     SnakeAndLaddersGame game = new SnakeAndLaddersGame();
     BoardGame boardGame = new BoardGame();
+    PlayerFileReader csvReader = new PlayerFileReader();
+
     MainPage.main(args);
     game.createDice(2);
     game.addPlayer(new Player("Arne", game));
@@ -14,11 +18,16 @@ public class Main {
     game.addPlayer(new Player("Majid", game));
     game.addPlayer(new Player("Atle", game));
 
+    csvReader.readCsvBuffered("playerInGameFile", boardGame);
+
     //For testing purposes.
+    /*
     boardGame.addPlayer(new Player("Arne", game));
     boardGame.addPlayer(new Player("Ivar", game));
     boardGame.addPlayer(new Player("Majid", game));
     boardGame.addPlayer(new Player("Atle", game));
+
+     */
     game.play();
   }
 }
