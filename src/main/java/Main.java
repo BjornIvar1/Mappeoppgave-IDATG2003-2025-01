37ba2f5730b6eea2be72fd3f6265d271f15aac1c
bjornivar <bihoie@stud.ntnu.no>
2025-03-30 19:16:27 +0200

    Tested playerFileReader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,5 @@
 import engine.BoardGame;
-import filehandler.BoardFileReaderGson;
-import filehandler.BoardFileReader;
-import filehandler.BoardFileWriter;
-import filehandler.BoardFileWriterGson;
+import filehandler.*;
 import model.Player;
 
 import java.io.IOException;
@@ -12,9 +9,10 @@ public class Main {
   public static void main(String[] args) {
     BoardFileReader reader = new BoardFileReaderGson();
     BoardFileWriter writer = new BoardFileWriterGson();
+    PlayerFileReader playerFileReader = new PlayerFileReader();
     try {
       BoardGame boardGame = new BoardGame();
-      boardGame.createBoard(reader.readBoard(Path.of("src/main/resources/board.json")));
+      boardGame.createBoard(reader.readBoard(Path.of("src/main/resources/board/board.json")));
       boardGame.createDice(1);
       boardGame.addPlayer(new Player("Arne", boardGame));
       boardGame.addPlayer(new Player("Ivar", boardGame));
@@ -22,10 +20,9 @@ public class Main {
       boardGame.addPlayer(new Player("Atle", boardGame));
       boardGame.play();
       BoardGame boardGame1 = new BoardGame();
-      writer.writeBoard(boardGame.getBoard(), Path.of("src/main/resources/boardWritten.json"));
-      boardGame1.createBoard(reader.readBoard(Path.of("src/main/resources/boardWritten.json")));
-      boardGame1.addPlayer(new Player("Majid", boardGame1));
-      boardGame1.addPlayer(new Player("Atle", boardGame1));
+      writer.writeBoard(boardGame.getBoard(), Path.of("src/main/resources/board/boardWritten.json"));
+      boardGame1.createBoard(reader.readBoard(Path.of("src/main/resources/board/boardWritten.json")));
+      playerFileReader.readCsvBuffered("src/main/resources/players/playersInGameFile.csv", boardGame1);
       boardGame1.createDice(1);
       System.out.println("NEW GAME");
       boardGame1.play();
