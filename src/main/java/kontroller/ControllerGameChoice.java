a3d3567510f2b079d5d92e1454a955e5440e567a
arpit <arpitsa@stud.ntnu.no>
2025-04-02 13:55:38 +0200

    Created ControllerGameChoice.

---
+++ src/main/java/kontroller/ControllerGameChoice.java
@@ -0,0 +1,9 @@
+package kontroller;
+
+public class ControllerGameChoice {
+  private SceneManager sceneManager;
+
+  public ControllerGameChoice(SceneManager sceneManager) {
+    this.sceneManager = sceneManager;
+  }
+}
