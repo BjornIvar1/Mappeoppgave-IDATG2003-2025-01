a811e2133c97545517c09b705c4234141cb49711
arpit <arpitsa@stud.ntnu.no>
2025-04-02 13:25:19 +0200

    Create a controller for LandingPage.
    One constructor added

---
+++ src/main/java/kontroller/ControllerLandingPage.java
@@ -0,0 +1,14 @@
+package kontroller;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ControllerLandingPage {
+  private SceneManager sceneManager;
+
+  public ControllerLandingPage(SceneManager sceneManager) {
+    this.sceneManager = sceneManager;
+  }
+
+
+}
