47e973567593cd1bccd8c77f7c17d12db75c26ad
arpit <arpitsa@stud.ntnu.no>
2025-04-06 17:08:39 +0200

    Added Javadoc and changed a private field.

--- src/main/java/kontroller/ControllerGameChoice.java
+++ src/main/java/kontroller/ControllerGameChoice.java
@@ -1,9 +1,25 @@
 package kontroller;
 
+/**
+ * Manages game selection interactions.
+ *
+ * <p>This controller facilitates navigation and scene management related to game choices.
+ * It allows the user to choose and start a game.</p>
+ *
+ * @author A. Sahoo, B.I. Høie
+ * @since 0.0.1
+ * @version 0.0.2
+ */
 public class ControllerGameChoice {
-  private SceneManager sceneManager;
+  private final SceneManager sceneManager;
 
+  /**
+   * Constructs a {@code ControllerGameChoice} with the specified scene manager.
+   *
+   * @param sceneManager the {@code SceneManager} responsible for managing scene transitions
+   */
   public ControllerGameChoice(SceneManager sceneManager) {
     this.sceneManager = sceneManager;
   }
 }
+
