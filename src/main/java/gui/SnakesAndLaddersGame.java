dd6dcb3d8c6b6ff00770bba29b3b5c2ba9bfb5d6
bjornivar <bihoie@stud.ntnu.no>
2025-04-07 14:14:06 +0200

    Made the board add a player to a tile Board updates when a player moves

--- src/main/java/gui/SnakesAndLaddersGame.java
+++ src/main/java/gui/SnakesAndLaddersGame.java
@@ -18,7 +18,7 @@ import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
+import model.Player;
 import java.io.IOException;
 import java.nio.file.Path;
 
@@ -36,6 +36,9 @@ public class SnakesAndLaddersGame extends Application {
   private static final int COLUMNS = 10;
   private BoardGame boardGame;
 
+  private VBox root;
+  private GridPane boardGrid;
+
   /**
    * Starts the JavaFX application and initializes the user interface.
    *
@@ -43,9 +46,10 @@ public class SnakesAndLaddersGame extends Application {
    */
   @Override
   public void start(Stage primaryStage) {
+    initializeGame();
     GridPane board = createBoard();
 
-    VBox root = new VBox();
+    root = new VBox();
     root.getChildren().addAll(board, createControlPanel());
 
     Scene scene = new Scene(root);
@@ -54,6 +58,11 @@ public class SnakesAndLaddersGame extends Application {
     primaryStage.show();
   }
 
+  /**
+   * Creates a menubar for the user to easily exit the game.
+   *
+   * @return menuBar.
+   */
   private void initializeGame() {
     boardGame = new BoardGame();
 
@@ -61,8 +70,11 @@ public class SnakesAndLaddersGame extends Application {
     PlayerFileReader playerReader = new PlayerFileReader();
     try {
       boardGame.createBoard(reader.readBoard(Path.of("src/main/resources/board/board.json")));
-      boardGame.createDice(9);
+      boardGame.createDice(1);
       playerReader.readCsvBuffered("src/main/resources/players/playersInGameFile.csv", boardGame);
+      for (Player player : boardGame.getPlayers()) {
+        player.placeOnTile(boardGame.getBoard().getTile(1));
+      }
     } catch (IOException e) {
       System.out.println("Could not read board or players from file: " + e.getMessage());
     }
@@ -79,15 +91,22 @@ public class SnakesAndLaddersGame extends Application {
     {
       System.out.println("Game started!");
       initializeGame();
+      updateBoard();
     });
 
     Button rollDice = new Button("Roll Dice");
     rollDice.setOnAction(e -> {
       boardGame.play();
+      updateBoard();
     });
 
     controlPanel.getChildren().addAll(startButton, rollDice);
     return controlPanel;
+  }
+
+  private void updateBoard() {
+    boardGrid = createBoard();
+    root.getChildren().set(0, boardGrid);
   }
 
   private Circle createPlayer() {
@@ -124,17 +143,21 @@ public class SnakesAndLaddersGame extends Application {
     } else {
       rect.setFill(Color.WHITE);
     }
+    if (boardGame.getBoard().getTiles().get(tileid).getLandAction() != null) {
+      rect.setFill(Color.BROWN);
+    }
     rect.setStroke(Color.BLACK);
     Text text = new Text(String.valueOf(tileid));
 
     StackPane stack = new StackPane();
     stack.getChildren().addAll(rect, text);
 
-    if (tileid == 75) {
-      Circle circle = createPlayer();
-      stack.getChildren().add(circle);
+    for (Player player : boardGame.getPlayers()) {
+      if (player.getCurrentTile().getTileId() == tileid) {
+        Circle playerCircle = createPlayer();
+        stack.getChildren().add(playerCircle);
+      }
     }
-
     return stack;
   }
 
