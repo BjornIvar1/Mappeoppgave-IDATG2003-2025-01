0836d69acd4e55935b1195fa79c6406e85a0a24f
bjornivar <bihoie@stud.ntnu.no>
2025-04-07 15:39:29 +0200

    Refactored main method to launch game through GameLauncher

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,7 @@
-import engine.BoardGame;
-import filehandler.*;
-import gui.MainPage;
-import model.Player;
-
-import java.io.IOException;
-import java.nio.file.Path;
+import gui.GameLauncher;
 
 public class Main {
   public static void main(String[] args) {
-    BoardFileReader reader = new BoardFileReaderGson();
-    BoardFileWriter writer = new BoardFileWriterGson();
-    PlayerFileReader playerFileReader = new PlayerFileReader();
-    try {
-      BoardGame boardGame = new BoardGame();
-      boardGame.createBoard(reader.readBoard(Path.of("src/main/resources/board/board.json")));
-      boardGame.createDice(1);
-      boardGame.addPlayer(new Player("Arne", boardGame));
-      boardGame.addPlayer(new Player("Ivar", boardGame));
-      boardGame.addPlayer(new Player("Majid", boardGame));
-      boardGame.addPlayer(new Player("Atle", boardGame));
-      boardGame.play();
-      BoardGame boardGame1 = new BoardGame();
-      writer.writeBoard(boardGame.getBoard(), Path.of("src/main/resources/board/boardWritten.json"));
-      boardGame1.createBoard(reader.readBoard(Path.of("src/main/resources/board/boardWritten.json")));
-      playerFileReader.readCsvBuffered("src/main/resources/players/playersInGameFile.csv", boardGame1);
-      boardGame1.createDice(1);
-      System.out.println("NEW GAME");
-      boardGame1.play();
-    } catch (IOException e) {
-      System.out.println("Could not read board from file: " + e.getMessage());
-    }
-    MainPage.main(args);
+    GameLauncher.main(args);
   }
 }
