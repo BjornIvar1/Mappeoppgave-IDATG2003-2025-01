2bb3f9aa038ad6d69e184322124e20420de81841
bjornivar <bihoie@stud.ntnu.no>
2025-04-10 16:09:41 +0200

    Updated BaseGamePage  - Added javaDoc for the class  - Added a method for displaying the players game piece in the game

--- src/main/java/gui/BaseGamePage.java
+++ src/main/java/gui/BaseGamePage.java
@@ -1,9 +1,20 @@
 package gui;
 
+import engine.BoardGame;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import model.Player;
 
+/**
+ * This class represents the base page for the game.
+ */
 public class BaseGamePage extends BasePage {
+  /**
+   * Creates a player game piece.
+   *
+   * @param playerColor the color of the player
+   * @return a Circle representing the player game piece
+   */
   protected Circle createPlayer(String playerColor) {
     Circle gamePiece = new Circle(15);
     Color color;
@@ -13,6 +24,12 @@ public class BaseGamePage extends BasePage {
     return gamePiece;
   }
 
+  /**
+   * Switch statement to get the color of the player.
+   *
+   * @param playerColor the color of the player
+   * @return the color of the player
+   */
   private Color getColor(String playerColor) {
     return switch (playerColor.toUpperCase()) {
       case "RED" -> Color.RED;
@@ -21,5 +38,21 @@ public class BaseGamePage extends BasePage {
       case "YELLOW" -> Color.YELLOW;
       default -> Color.BLACK;
     };
+  }
+
+  /**
+   * Displays the players in the game.
+   *
+   * @param boardGame the board game
+   * @return a string representation of the players
+   */
+  protected String displayPlayers(BoardGame boardGame) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (Player player : boardGame.getPlayers()) {
+      stringBuilder.append("Name: ").append(player.getName()).append("; ")
+          .append("Game Piece: ").append(player.getColor())
+          .append("\n");
+    }
+    return stringBuilder.toString();
   }
 }
