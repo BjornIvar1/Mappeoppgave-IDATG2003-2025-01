0ae67eb17c33931ecd69961cbb68e7a9413b54da
bjornivar <bihoie@stud.ntnu.no>
2025-04-10 14:23:43 +0200

    Add player piece creation functionality with color handling

--- src/main/java/gui/BaseGamePage.java
+++ src/main/java/gui/BaseGamePage.java
@@ -1,6 +1,38 @@
 package gui;
 
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
 public class BaseGamePage extends BasePage {
   //TODO get menu bare from basePage...
-  //Class for common functionality for all game pages
+  protected Circle createPlayer(String playerColor) {
+    Circle gamePiece = new Circle(15);
+    Color color;
+
+    color = getColor(playerColor);
+    gamePiece.setFill(color);
+    return gamePiece;
+  }
+
+  private Color getColor(String playerColor) {
+    Color color;
+    switch (playerColor.toUpperCase()) {
+      case "RED":
+        color = Color.RED;
+        break;
+      case "BLUE":
+        color = Color.BLUE;
+        break;
+      case "GREEN":
+        color = Color.GREEN;
+        break;
+      case "YELLOW":
+        color = Color.YELLOW;
+        break;
+      default:
+        color = Color.BLACK;
+        break;
+    }
+    return color;
+  }
 }
