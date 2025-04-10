0276af085ccf02a904badd30c05b073b2d660b97
arpit <arpitsa@stud.ntnu.no>
2025-04-10 11:11:23 +0200

    Add documentation for TileAction methods and remove unused money handling methods

--- src/main/java/model/TileAction.java
+++ src/main/java/model/TileAction.java
@@ -4,7 +4,7 @@ package model;
  * Represent an action when a player lands on a model.Tile.
  *
  * @author A. Sahoo, B.I. Høie
- * @version 0.3.0
+ * @version 0.3.1
  * @since 0.0.1
  */
 public interface TileAction {
@@ -15,11 +15,18 @@ public interface TileAction {
    */
   void perform(Player player);
 
+  /**
+   * Accesses a description of what happens when the player lands on a tile.
+   *
+   * @return a description.
+   */
   String getDescription();
 
+  /**
+   * Accesses the tile the player will be moved to.
+   *
+   * @return destinationTile of where the player will be moved.
+   */
   int getDestinationTile();
 
-  int getMoney();
-
-  int looseMoney(int money);
 }
