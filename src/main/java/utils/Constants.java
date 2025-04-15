b576f5dc56945fedd834e1146df975b2e6eabc99
bjornivar <bihoie@stud.ntnu.no>
2025-04-15 21:24:29 +0200

    Add Constants class for application-wide constants

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,26 @@
+package utils;
+
+/**
+ * This class contains constants used throughout the application.
+ *
+ * @author A. Sahoo, B.I. Høie
+ * @since 0.0.1
+ * @version 0.0.2
+ */
+public final class Constants {
+  // Prevent instantiation
+  private Constants() {}
+
+  public static final String SNAKES_AND_LADDERS_BOARD_FILE_PATH =
+      "src/main/resources/board/SnakesAndLaddersBoard.json";
+  public static final String SNAKES_AND_LADDERS_PLAYER_FILE_PATH =
+      "src/main/resources/players/playersInGameFile.csv";
+  public static final int SNAKES_AND_LADDERS_TILE_SIZE = 60;
+
+  public static final String COLOR_TILE_EVEN = "#32bff5";
+  public static final String COLOR_TILE_ODD = "#bbd962";
+
+  public static final String LABEL_START_GAME_BUTTON = "Start Game";
+  public static final String LABEL_ROLL_DICE_BUTTON = "Roll Dice";
+  public static final String LABEL_LAST_ROLLED_BUTTON = "Last rolled: ---";
+}
