2597d0c9a13c21502dd875816332e2f76e1ce6b7
arpit <arpitsa@stud.ntnu.no>
2025-04-16 19:05:34 +0200

    Refactor BaseGamePage to improve method documentation and clean up unused control panel methods

--- src/main/java/gui/BaseGamePage.java
+++ src/main/java/gui/BaseGamePage.java
@@ -3,11 +3,7 @@ package gui;
 import engine.BoardGame;
 import filehandler.BoardFileReaderGson;
 import filehandler.PlayerFileReader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -20,7 +16,7 @@ import java.nio.file.Path;
  *
  * @since 0.0.1
  * @author A. Sahoo, B.I. Høie
- * @version 0.3.0
+ * @version 0.3.1
  */
 public class BaseGamePage extends BasePage {
 
@@ -71,6 +67,16 @@ public class BaseGamePage extends BasePage {
     return stringBuilder.toString();
   }
 
+  /**
+   * Initializes the board game with players and board.
+   *
+   * <p>This method reads the board and player files, creates the board,
+   * and initializes the players.<p>
+   *
+   * @param boardFilePath of the board file.
+   * @param playerFilePath of the player file.
+   * @return the initialized board game.
+   */
   protected BoardGame initializeBoardGame(String boardFilePath, String playerFilePath) {
     BoardGame boardGame = new BoardGame();
     BoardFileReaderGson reader = new BoardFileReaderGson();
@@ -86,25 +92,10 @@ public class BaseGamePage extends BasePage {
     return boardGame;
   }
 
-  protected HBox createControlPanel(Button startButton, Button rollDice, Label gameInfo, Label playerInfo) {
-    HBox controlPanel = new HBox();
-    controlPanel.setPadding(new Insets(10));
-    controlPanel.setSpacing(10);
-    controlPanel.setAlignment(Pos.CENTER);
-
-    controlPanel.getChildren().addAll(startButton, rollDice, gameInfo, playerInfo);
-    return controlPanel;
-  }
-
-  protected Button getButton(String text, Runnable action) {
-    Button button = new Button(text);
-    button.setOnAction(e -> action.run());
-    return button;
-  }
-
   protected static Color getColor(int tileId) {
     return (tileId % 2 == 0) ? Color.web("#32bff5") : Color.web("#bbd962");
   }
+
   protected static void setFill(Rectangle rect, Color baseColor) {
     rect.setFill(baseColor);
   }
@@ -122,7 +113,7 @@ public class BaseGamePage extends BasePage {
     Player player = boardGame.getCurrentPlayer();
     int rollSum = boardGame.getDice().getDie(0) + boardGame.getDice().getDie(1);
     if (boardGame.getCurrentPlayer().getCurrentTile().getTileId() == 90) {
-      //TODO find a way to difine a winner by checking max balance.
+      //TODO find a way to define a winner by checking max balance.
       //TODO add if sentence of which game the player is playing.
       gameInformation.setText("Winner: " + player.getName() + "\n" + "Press Start Game to play again");
     } else {
