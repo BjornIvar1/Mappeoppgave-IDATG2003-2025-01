7ad6afa06c88c18c3acdadbc492246ffd5694907
bjornivar <bihoie@stud.ntnu.no>
2025-04-21 17:07:25 +0200

    Created controller for CreateUserPage

---
+++ src/main/java/controller/ControllerCreateUser.java
@@ -0,0 +1,16 @@
+package controller;
+
+import gui.menu.GameSelection;
+
+public class ControllerCreateUser {
+  private final SceneManager sceneManager;
+
+  /**
+   * Constructs a {@code ControllerCreateUser} with a specified scene manager.
+   *
+   * @param sceneManager the {@code SceneManager} responsible for handling scene transitions
+   */
+  public ControllerCreateUser(SceneManager sceneManager) {
+    this.sceneManager = sceneManager;
+  }
+}
