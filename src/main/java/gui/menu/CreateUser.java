1f48938806d29c5da595de50b3d161d19c93d025
bjornivar <bihoie@stud.ntnu.no>
2025-04-21 18:02:01 +0200

    early development Implement user creation functionality in CreateUser class

--- src/main/java/gui/menu/CreateUser.java
+++ src/main/java/gui/menu/CreateUser.java
@@ -1,20 +1,47 @@
 package gui.menu;
 
 import controller.ControllerCreateUser;
+import filehandler.PlayerFileWriter;
 import gui.BasePage;
 import javafx.geometry.Pos;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.TextField;
+import model.Player;
 
 public class CreateUser extends BasePage {
   private final ControllerCreateUser controller;
 
-  public CreateUser(ControllerCreateUser controller) {
-    this.controller = controller;
-    setAlignment(Pos.CENTER);
-    BorderPane mainLayout = new BorderPane();
+  public CreateUser(ControllerCreateUser controllerGameChoice) {
+    this.controller = controllerGameChoice;
+    setCenter(createUserPane());
+  }
 
-    mainLayout.setTop(createMenuBar());
+  private Pane createUserPane() {
+    VBox container = new VBox();
+    TextField textField = new TextField();
+    textField.setPromptText("Enter your name");
+    textField.setMaxWidth(200);
+    Alert alertWarning = new Alert(Alert.AlertType.WARNING);
+    Alert alertConfirmation = new Alert(Alert.AlertType.CONFIRMATION);
 
-    getChildren().add(mainLayout);
+    Button createUserButton = new Button("Create User");
+    createUserButton.setOnAction(event -> {
+      try {
+        String userName = textField.getText();
+        Player player = new Player(userName, "red", null);
+        alertConfirmation.setContentText("User " + player.getName() + " created successfully!");
+        alertConfirmation.show();
+      } catch (IllegalArgumentException e) {
+        alertWarning.setContentText(e.getMessage());
+        alertWarning.show();
+      }
+    });
+
+    container.getChildren().addAll(textField, createUserButton);
+    container.setAlignment(Pos.CENTER);
+    return container;
   }
 }
