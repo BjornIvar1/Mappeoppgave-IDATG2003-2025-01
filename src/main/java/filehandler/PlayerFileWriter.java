4206fe44b76e92ef0db7604a0a1a61e8e15e1c52
arpit <arpitsa@stud.ntnu.no>
2025-05-18 19:58:14 +0200

    Refactor PlayerFileWriter to improve method naming and code formatting

--- src/main/java/filehandler/PlayerFileWriter.java
+++ src/main/java/filehandler/PlayerFileWriter.java
@@ -1,23 +1,22 @@
 package filehandler;
 
-import model.Player;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import model.Player;
 
 /**
  * PlayerFileWriter writes and saves all the players that are stored in the game.
  *
  * @author A. Sahoo, B.I. Høie
- * @version 0.1.1
+ * @version 0.1.2
  * @since 0.0.1
  */
 public class PlayerFileWriter {
 
-  private PlayerFileWriter () {}
+  private PlayerFileWriter() {}
 
   /**
    * Write a CSV file from a list.
@@ -25,16 +24,13 @@ public class PlayerFileWriter {
    * @param playersToSave The list of players.
    * @param fileName The file name.
    */
-  public static void writeToCSV(List<Player> playersToSave, String fileName) {
+  public static void writeToCsv(List<Player> playersToSave, String fileName) {
     try (BufferedWriter writer = Files.newBufferedWriter(Path.of(fileName))) {
       for (Player player : playersToSave) {
-        System.out.println("Writing player: " + player.getName() + ", Balance: " + player.getBalance());
-        //System out for debugging.
         writer.write(player.getName() + "," + player.getColor() + "," + player.getBalance() + "\n");
       }
     } catch (IOException e) {
       System.out.println("Error writing to file: " + fileName);
     }
   }
-  //TODO : Add a method to save the players to a file with balance.
 }
