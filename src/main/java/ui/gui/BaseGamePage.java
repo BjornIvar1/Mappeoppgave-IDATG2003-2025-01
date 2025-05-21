e5b77bfd6e56ed05507d85def557874a54bac08c
arpit <arpitsa@stud.ntnu.no>
2025-05-21 14:50:46 +0200

    Refactor getColor method and remove unused createTopBar method in BaseGamePage

--- src/main/java/ui/gui/BaseGamePage.java
+++ src/main/java/ui/gui/BaseGamePage.java
@@ -2,12 +2,6 @@ package ui.gui;
 
 import engine.BoardGame;
 import java.util.Iterator;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import model.Player;
@@ -17,7 +11,7 @@ import model.Player;
  *
  * @since 0.0.1
  * @author A. Sahoo, B.I. Høie
- * @version 0.4.4
+ * @version 0.4.5
  */
 public class BaseGamePage extends BasePage {
 
@@ -50,6 +44,19 @@ public class BaseGamePage extends BasePage {
       case "YELLOW" -> Color.YELLOW;
       default -> Color.BLACK;
     };
+  }
+
+  /**
+   * Gets the color of the tile based on its ID.
+   *
+   * <p>This method returns a color based on the tile ID.
+   * Even tiles are blue and odd tiles are green.</p>
+   *
+   * @param tileId the ID of the tile
+   * @return the color of the tile
+   */
+  protected static Color getColor(int tileId) {
+    return (tileId % 2 == 0) ? Color.web("#32bff5") : Color.web("#bbd962");
   }
 
   /**
@@ -93,44 +100,5 @@ public class BaseGamePage extends BasePage {
     return stringBuilder.toString();
   }
 
-  /**
-   * Gets the color of the tile based on its ID.
-   *
-   * <p>This method returns a color based on the tile ID.
-   * Even tiles are blue and odd tiles are green.</p>
-   *
-   * @param tileId the ID of the tile
-   * @return the color of the tile
-   */
-  protected static Color getColor(int tileId) {
-    return (tileId % 2 == 0) ? Color.web("#32bff5") : Color.web("#bbd962");
-  }
-
-
-  /**
-   * Creates a top bar with return and game rules buttons.
-   *
-   * <p>This method creates a top bar with two buttons: one to return
-   * to the previous page and another to show game rules.</p>
-   *
-   * @param returnButton the button to return to the previous page
-   * @param gameRulesButton the button to show game rules
-   * @return a HBox containing the buttons
-   */
-  protected HBox createTopBar(Button returnButton, Button gameRulesButton) {
-    HBox topBar = new HBox();
-    topBar.setPadding(new Insets(10));
-    topBar.setSpacing(10);
-    topBar.setAlignment(Pos.CENTER);
-
-    Region spacer = new Region();
-    HBox.setHgrow(spacer, Priority.ALWAYS);
-
-    returnButton.setMaxWidth(Double.MAX_VALUE);
-    gameRulesButton.setMaxWidth(Double.MAX_VALUE);
-
-    topBar.getChildren().addAll(returnButton, spacer, gameRulesButton);
-    return topBar;
-  }
 
 }
