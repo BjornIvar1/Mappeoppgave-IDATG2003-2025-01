20cf0733c0fa76a7c9e642f23911e23cd8e2b5a7
bjornivar <bihoie@stud.ntnu.no>
2025-05-21 15:31:04 +0200

    Enhanced the JavaDoc Fixed SonarQube and CheckStyle Violations

--- src/main/java/filehandler/board/BoardFileWriter.java
+++ src/main/java/filehandler/board/BoardFileWriter.java
@@ -1,12 +1,25 @@
 package filehandler.board;
 
-import model.Board;
-import model.Player;
-
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
+import model.Board;
 
+/**
+ * An interface for writing a {@link Board} object to a file.
+ *
+ * <p>This interface defines a method for writing a board to a JSON file.</p>
+ *
+ * @author A. Sahoo, B.I. Høie
+ * @version 0.2.1
+ * @since 0.0.1
+ */
 public interface BoardFileWriter {
+  /**
+   * Writes a board to a file.
+   *
+   * @param board the board to write
+   * @param filePath the path to the file
+   * @throws IOException if an I/O error occurs
+   */
   void writeBoard(Board board, Path filePath) throws IOException;
 }
