9d293eee49409444cecf411918f7262ae7b0a38b
arpit <arpitsa@stud.ntnu.no>
2025-05-21 14:51:04 +0200

    Add createTopBar method to BasePage for navigation and game rules buttons

--- src/main/java/ui/gui/BasePage.java
+++ src/main/java/ui/gui/BasePage.java
@@ -1,10 +1,12 @@
 package ui.gui;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 
 /**
  * This class represents the base page for the application.
@@ -12,10 +14,15 @@ import javafx.scene.layout.Pane;
  *
  * @author A. Sahoo, B.I. Høie
  * @since 0.0.1
- * @version 0.1.0
+ * @version 0.2.0
  */
 public class BasePage extends BorderPane {
 
+  /**
+   * Creates a new instance of BasePage.
+   *
+   * <p>This constructor initializes the top menu bar for the page.</p>
+   */
   public BasePage() {
     setTop(createMenuBar());
   }
@@ -38,6 +45,39 @@ public class BasePage extends BorderPane {
     return menuBar;
   }
 
+  /**
+   * Creates a top bar with return and game rules buttons.
+   *
+   * <p>This method creates a top bar with two buttons: one to return
+   * to the previous page and another to show game rules.</p>
+   *
+   * @param returnButton the button to return to the previous page
+   * @param gameRulesButton the button to show game rules
+   * @return a HBox containing the buttons
+   */
+  protected HBox createTopBar(Button returnButton, Button gameRulesButton) {
+    HBox topBar = new HBox();
+    topBar.setPadding(new Insets(10));
+    topBar.setSpacing(10);
+    topBar.setAlignment(Pos.CENTER);
+
+    Region spacer = new Region();
+    HBox.setHgrow(spacer, Priority.ALWAYS);
+
+    returnButton.setMaxWidth(Double.MAX_VALUE);
+    gameRulesButton.setMaxWidth(Double.MAX_VALUE);
+
+    topBar.getChildren().addAll(returnButton, spacer, gameRulesButton);
+    return topBar;
+  }
+
+  /**
+   * Sets the content of the page.
+   *
+   * <p>This method sets the center of the page to the specified content.</p>
+   *
+   * @param content the content to be set in the center of the page.
+   */
   protected void setPageContent(Pane content) {
     setCenter(content);
   }
